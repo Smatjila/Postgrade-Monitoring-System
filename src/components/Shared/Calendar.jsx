829f6e35680a17736bdcfe06ef5eb0c49992c98b
KatlehoMadaba <katlehomadaba44@gmail.com>
2024-07-29 01:06:55 +0200

    In the middle of commiting Posting annoucements

--- src/components/Shared/Calendar.jsx
+++ src/components/Shared/Calendar.jsx
@@ -7,12 +7,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Swal from 'sweetalert2';
 import { useAuth } from "../../Backend/AuthContext";
-import { getDocs, query, collection,where} from 'firebase/firestore'; // Import Firestore functions
-import { db,auth} from '../../Backend/Config';
+import { getDocs, query, collection, where } from 'firebase/firestore'; // Import Firestore functions
+import { db, auth } from '../../Backend/Config';
 
 function Calendar() {
   const { CurrentUser } = useAuth();
-  const SupervisorID = 720000007; // Replace this with the actual SupervisorID dynamically if needed
+  const [SupervisorID, setSupervisorID] = useState(null);
   const [events, setEvents] = useState([
     { title: 'Software Testing', start: '2024-07-01', end: '2024-07-15', backgroundColor: '#378006', borderColor: '#378006' },
     { title: 'Research Paper Calendar', start: '2024-07-02', end: '2024-07-05', backgroundColor: '#ff9f89', borderColor: '#ff9f89' }
@@ -21,51 +21,67 @@ function Calendar() {
   const [moduleTitles, setModuleTitles] = useState([]);
 
   useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setSupervisorID(user.email.substring(0, 9));
+        console.log(user.email.substring(0,9));
+      } else {
+        setSupervisorID(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
     const fetchModules = async () => {
-      try{
-        const q = query(collection(db, 'Module'), where('SupervisorID', '==', SupervisorID));
+      if (!SupervisorID) return;
+
+      try {
+
+        const q = query(collection(db, 'Module'), where('SupervisorID', '==', Math.floor(SupervisorID)));
         const querySnapshot = await getDocs(q);
         const modulesArray = [];
         querySnapshot.forEach((doc) => {
           modulesArray.push(doc.data().ModuleTitle);
         });
         setModuleTitles(modulesArray);
+      } catch (error) {
+        console.error("Error fetching modules:", error);
       }
-      catch(error){
-        console.error("error fetch moudles:", error);
-      };
-      }
- 
+    };
+
     fetchModules();
+    // console.log(SupervisorID+"this is the id afterr");
   }, [SupervisorID]);
 
   const handleDateClick = (arg) => {
     setNewEvent({ ...newEvent, start: arg.dateStr, end: arg.dateStr });
   };
 
-    const showEventPopup = async () => {
-      const moduleOptions = moduleTitles.map((title, index) => `<option value="${title}">${title}</option>`).join('');
-      const { value: formValues } = await Swal.fire({
-        title: 'Add Event',
-        html: `
-          <input id="swal-input1" class="swal2-input" placeholder="Event Title">
-          <input id="swal-input2" class="swal2-input" type="date" value="${newEvent.start}">
-          <input id="swal-input3" class="swal2-input" type="date" value="${newEvent.end}">
-          <select id="swal-input5" class="swal2-input">
+  const showEventPopup = async () => {
+    const moduleOptions = moduleTitles.map((title) => `<option value="${title}">${title}</option>`).join('');
+    const { value: formValues } = await Swal.fire({
+      title: 'Add Event',
+      html: `
+        <input id="swal-input1" class="swal2-input" placeholder="Event Title">
+        <input id="swal-input2" class="swal2-input" type="date" value="${newEvent.start}">
+        <input id="swal-input3" class="swal2-input" type="date" value="${newEvent.end}">
+        <select id="swal-input4" class="swal2-input">
           <option value="" disabled selected>Select a color</option>
           <option value="#378006" style="background-color: #378006; color: #378006;">● Green</option>
           <option value="#ff9f89" style="background-color: #ff9f89; color: #ff9f89;">● Pink</option>
           <option value="#1e90ff" style="background-color: #1e90ff; color: #1e90ff;">● Blue</option>
           <option value="#ff6347" style="background-color: #806CA5; color: #806CA5;">● Purple</option>
           <option value="#ffa500" style="background-color: #ffa500; color: #ffa500;">● Orange</option>
-          </select>
-          <select id="swal-input5" class="swal-input">
+        </select>
+        <select id="swal-input5" class="swal2-input">
           <option value="" disabled selected>Select a module</option>
           ${moduleOptions}
-          </select>
+        </select>
       `,
       focusConfirm: false,
-      preConfirm: () => { 
+      preConfirm: () => {
         return [
           document.getElementById('swal-input1').value,
           document.getElementById('swal-input2').value,
@@ -77,7 +93,7 @@ function Calendar() {
     });
 
     if (formValues) {
-      const [title, start, end, module, color] = formValues;
+      const [title, start, end, color, module] = formValues;
       setEvents([...events, { title, start, end, module, backgroundColor: color, borderColor: color }]);
       Swal.fire('Event Added', JSON.stringify(formValues));
     }
--- src/components/SupervisorComponents/SupRightSidebar.jsx
+++ src/components/SupervisorComponents/SupRightSidebar.jsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 import Right from '../../assets/icons/Right-icon.png';
 import NotificationsPopupModal from '../StudentComponents/NotificationModal';
 import "../../../src/assets/styles.css";
+import { useAuth } from "../../Backend/AuthContext";
+import { getDocs, query, collection, where } from 'firebase/firestore'; // Import Firestore functions
+import { db, auth } from '../../Backend/Config';
+
 const SupervisorNotificationsSidebar = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [inputValue,setInputValue]=useState('')
-  const[selectedOption,setSelectedOption]=useState('');
-
-
+const {CurentUser}=useAuth();
+const [supervisorID,setSupervisorID]=useState(null);
+const [showModal, setShowModal] = useState(false);
+const [inputValue,setInputValue]=useState('')
+const[selectedOption,setSelectedOption]=useState('');
   const notifications = [
     { id: 1, supervisor: "Dr. Amanda Green", message: "Reminder: Please review and approve the software design proposal by the end of this week.", date: "2021-09-15", time: "10:00"},
     { id: 2, supervisor: "Prof. David White", message: "The meeting scheduled for next Monday has been postponed to Thursday.", date: "2021-09-15", time: "10:00"},
@@ -17,6 +21,40 @@ const SupervisorNotificationsSidebar = () => {
     { id: 4, supervisor: "Prof. David White", message: "The meeting scheduled for next Monday has been postponed to Thursday.", date: "2021-09-15", time: "10:00"},
     { id: 5, supervisor: "Dr. Amanda Green", message: "Please review and provide feedback on the final project report draft by Friday.", date: "2021-09-15", time: "10:00"}
   ];
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setSupervisorID(user.email.substring(0, 9));
+        console.log(user.email.substring(0,9));
+      } else {
+        setSupervisorID(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+  
+  useEffect(() => {
+    const fetchModules = async () => {
+      if (!SupervisorID) return;
+
+      try {
+
+        const q = query(collection(db, 'Module'), where('SupervisorID', '==', Math.floor(SupervisorID)));
+        const querySnapshot = await getDocs(q);
+        const modulesArray = [];
+        querySnapshot.forEach((doc) => {
+          modulesArray.push(doc.data().ModuleTitle);
+        });
+        setModuleTitles(modulesArray);
+      } catch (error) {
+        console.error("Error fetching modules:", error);
+      }
+    };
+
+    fetchModules();
+    // console.log(SupervisorID+"this is the id afterr");
+  }, [SupervisorID]);
 
   const openModal = () => {
     setShowModal(true);
